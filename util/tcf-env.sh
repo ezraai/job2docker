7409b389f86a219518bc385158014c1f891008d1
Edward Ost <eost@talend.com>
2019-03-04 14:33:49 -0500

    updated

--- util/tcf-env.sh
+++ util/tcf-env.sh
@@ -8,14 +8,14 @@ export PARSE_TCF_ENV_FLAG=1
 
 # set TALEND_STUDIO_RESOURCES to 'true' to control include path and file name suffix
 
-if [ "${TALEND_STUDIO_RESOURCES}" == "true" ]; then
+if [ "${TALEND_STUDIO_RESOURCES:-}" == "true" ]; then
     # studio resource configuration puts everything in the same directory
     # and appends a version to the file name
     export RESOURCE_VERSION="_0.1"
-    export UTIL_DIR_RELATIVE_PATH=""
+    export UTIL_RELATIVE_PATH=""
 else
     # bash configuration is the default and uses separate util directory and has no version suffix
     export RESOURCE_VERSION=""
-    export UTIL_DIR_RELATIVE_PATH="../util/"
+    export UTIL_RELATIVE_PATH="../util/"
 fi
 
